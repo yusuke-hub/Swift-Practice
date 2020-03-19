e9dce1c5ca2fe7feb363d99135dcbfc6b62932c3
yusuke-hub <yk1210140408@gmail.com>
2020-03-19 16:34:22 +0900

    計算のプログラムを記述

--- Swift5CountUp/Swift5CountUp/ViewController.swift
+++ Swift5CountUp/Swift5CountUp/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus(_ sender: Any) {
+        
+        count = count + 1
 
+        label.text = String(count)
+        
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        
+        count = count - 1
+        
+        label.text = String(count)
+    }
 }
 
