8d50e03a549684ad713d5623f87a3f2ff8e7578c
yusuke-hub <yk1210140408@gmail.com>
2020-03-19 15:50:34 +0900

    imageViewとButtonを配置

---
+++ Swift5ImageView1/Swift5ImageView1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Swift5ImageView1
+//
+//  Created by 川端雄介 on 2020/03/19.
+//  Copyright © 2020 Yusuke Kawabata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    Ctrl押しながらドラッグ&ドロップ
+    @IBOutlet var imageView: UIImageView!
+    
+    var count = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func changeImage(_ sender: Any) {
+        
+//        もしtapが10回されたら
+//        imageViewのimageをbackgroundに変える
+        
+    }
+    
+}
+
