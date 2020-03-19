52b1d13581cbe03616319a80fc7cf8cd4bcac746
yusuke-hub <yk1210140408@gmail.com>
2020-03-19 10:39:29 +0900

    変数を宣言

--- SwiftButton1/SwiftButton1/ViewController.swift
+++ SwiftButton1/SwiftButton1/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //countという変数の中に5が入っています
+    //countという変数を下で使うことができるようになりました
+    var count = 5
+    
+    var moji = "あいうえお"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         //　勝手に呼ばれる場所
+        
+        count = 10
+        print(count)
     }
     @IBAction func tap(_ sender: AnyObject) {
         // ボタンを押して離した時の処理
