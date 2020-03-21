b0adb20c9124c17aabd5bc0ccde112108b26e256
yusuke-hub <yk1210140408@gmail.com>
2020-03-22 08:48:42 +0900

    SoundViewController.swiftと部品を紐付け

--- SwiftMousouLINE1/SwiftMousouLINE1/SoundViewController.swift
+++ SwiftMousouLINE1/SwiftMousouLINE1/SoundViewController.swift
@@ -7,16 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
+    @IBOutlet var timeLabel: UILabel!
+    
+    @IBOutlet var imageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        画面が表示された時に画像を非表示
+        imageView2.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func tap(_ sender: Any) {
+        // imageView2を表示させる
+        
+        // タイマーをスタートさせてラベルへ秒数を表示する
+        // baby.mp3の音声を再生する
+    }
+    
+    @IBAction func deny(_ sender: Any) {
+        // 電源を切る
+        // 音声をstopする
+        //  画面をViewControllerへ戻す
+    }
     /*
     // MARK: - Navigation
 
