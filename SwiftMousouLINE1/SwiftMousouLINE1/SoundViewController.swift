9bdcbe2f75b79cfaef90a5bb2897ba924e5fbdec
yusuke-hub <yk1210140408@gmail.com>
2020-03-21 15:21:29 +0900

    コール画面とプログラムを紐付け

---
+++ SwiftMousouLINE1/SwiftMousouLINE1/SoundViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SoundViewController.swift
+//  SwiftMousouLINE1
+//
+//  Created by 川端雄介 on 2020/03/21.
+//  Copyright © 2020 Yusuke Kawabata. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
