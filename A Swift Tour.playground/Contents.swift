bdeecb614130284c17b6b8b623f00751438ebdc3
yusuke-hub <yk1210140408@gmail.com>
2020-03-22 13:56:49 +0900

    ~L125

--- A Swift Tour.playground/Contents.swift
+++ A Swift Tour.playground/Contents.swift
@@ -53,4 +53,75 @@ for score in individualScores {
 var optionalString: String? = "Hello"
 print(optionalString == nil)
 
-var
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello,\(name)"
+}
+
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+// hasPrefix:接頭が一致
+// hasSuffix:接尾が一致
+
+let vegetable = "tomato"
+switch vegetable {
+case "celery":
+print("Add some raisins and make ants on a log.")
+case "cucumber":
+print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+let interestingNumbers = [
+    "Prime":[2,3,5,7,11,13],
+    "Fibonacci": [1,1,2,3,5,8],
+    "Square": [1,4,9,16,25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+
+//while文:ループ回数が未定の時に使用する事が多い.無限ループを作らないように注意
+var n = 3
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for i in 0..<7 {
+    total += i
+}
+print(total)
+
+//文字列における変数展開 → \(変数)
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet(person: "Bob", day: "Tuesday")
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet(person: "John", day: "Wednesday")
+
+
