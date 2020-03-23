17e221be4f6a11c3aec928c846b97c8630549152
yusuke-hub <yk1210140408@gmail.com>
2020-03-23 10:12:25 +0900

    ~ L141

--- dotinstall:swift-practice.playground/Contents.swift
+++ dotinstall:swift-practice.playground/Contents.swift
@@ -57,3 +57,85 @@ print("y2 is \(y * 2)")
 print(true && false)
 print(true || false)
 print(!true)
+
+//  ★条件分岐
+let score = 82
+let result: String
+
+//if score > 80 {
+//    result = "great"
+//} else if score > 60 {
+//    result = "good"
+//} else {
+//    result = "so so ..."
+//}
+
+result = score > 80 ? "great" : "so so ..."
+print(result)
+
+// Switch文
+let num = 5
+switch num {
+case 0:
+    print("zero")
+case 1, 2, 3:
+    print("small")
+case 4...6:
+    print("4/5/6")
+case 7..<9:
+    print("7/8")
+case let n where n > 20:
+    print("\(n) is hyge!")
+default:
+    print("n.a.")
+    break
+}
+
+//while文
+var n = 0
+//while n < 3 {
+//    print(n)
+//    n += 1
+//}
+
+// repeat/while文
+//条件に該当じなくても1回は必ず処理が実行される
+repeat {
+    print(n)
+    n += 1
+} while n < 3
+
+// for文
+// for 変数 in データの集合 { 処理 }
+// break: 以降の処理が止まる
+// continue: その時点でのループ処理を飛ばして、以降の処理を事項する
+for i in 0...5 {
+    print(i)
+}
+for i in 0...5 {
+    if i == 3 {
+        break
+    }
+    print(i)
+}
+for i in 0...5 {
+    if i == 3 {
+        continue
+    }
+    print(i)
+}
+
+// ★nil
+//let s: String = nil
+//let os: Optional<String> = nil
+
+let os: String? = nil
+if os != nil {
+    print(os!) //unwrap
+}
+
+// Optional Binding
+// もしosがnilじゃなかったら、osにvalueを代入する
+if let value = os {
+    print(value)
+}
